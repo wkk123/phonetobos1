5ac345ca611b78276a55e03da77151adaa140879
王康康 <1754393348@qq.com>
2016-09-07 11:18:05 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,75 @@
+var todo=angular.module('todo',['ngAnimate']);
+todo.controller('mian',['$scope','$timeout',function ($scope,$timeout) {
+    $scope.title="Todos";
+   /* setInterval(function () {
+        $timeout(function () {
+            var date=new Date();
+            var h=date.getHours();
+            var m=date.getMinutes();
+            var s=date.getSeconds();
+            s=(s<10)?('0'+s):s;
+            $scope.title=h+':'+m+':'+s;
+        },0)
+    },500)*/
+    $scope.name='';
+    $scope.todos=[];
+    if(localStorage.shuju){
+        $scope.todos=JSON.parse(localStorage.shuju);
+    }else{
+        $scope.todos=[];
+    }
+    $scope.save=function () {
+        localStorage.shuju=JSON.stringify($scope.todos);
+    }
+
+    $scope.delete=function (id) {
+        var index ;
+        for(var i=0;i<$scope.todos.length;i++){
+            if($scope.todos[i].id===id){
+                index=i;
+            }
+        }
+        $scope.todos.splice(index,1);
+    }
+        $scope.focus=function (e) {
+            $timeout(function () {
+            $(e.currentTarget)
+                .find('input')
+                .trigger('focus');
+            },0)
+        }
+    $scope.add=function (e) {
+        if(e.keyCode === 13){
+            if($scope.todos.length===0){
+                 var id=0;
+            }else {
+            var max=-Infinity;
+            for(var i=0;i<$scope.todos.length;i++){
+                var value=$scope.todos[i];
+                if(value.id>max){
+                    max=value.id;
+                }
+
+            }
+            var id=max+1;
+        }
+                if($scope.name===''){
+                    return;
+                }
+                $scope.todos.unshift({id:id,name:$scope.name,isDone:false});
+                console.table($scope.todos)
+                $scope.name="";
+        }
+    }
+    $scope.clear=function () {
+        var arr=[];
+        for(var i=0;i<$scope.todos.length;i++){
+            if(!$scope.todos[i].isDone){
+                arr.push($scope.todos[i]);
+            }
+
+        }
+        $scope.todos=arr;
+    }
+
+}])
